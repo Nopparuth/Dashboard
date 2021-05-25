1764bd76d238a6ef626d084d671a0948c114cf56
nopparut hempamorn <nopparuthempamorn@nopparuts-MacBook-Pro.local>
2021-05-25 12:44:03 +0700

    first commit

---
+++ components/justsay.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../components/Card";
+
+const JustSay = () => {
+  return (
+    <div>
+      <Card>
+        <h2 className="text-lg font-bold text-gray-400 mb-1.5">JustSay</h2>
+        <div className="text-center mt-8 mb-12">
+          <h1 className="text-4xl font-bold undefined">Hello</h1>
+        </div>
+      </Card>
+    </div>
+  );
+};
+export default JustSay;
