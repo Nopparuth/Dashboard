900cc967106860816e07b2a35a9b4fe833fc2848
nopparut hempamorn <nopparuthempamorn@nopparuts-MacBook-Pro.local>
2021-05-28 17:42:08 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '/style.css'
+// pages/_app.js
+import React from 'react';
+import App from 'next/app';
+import MainLayout from '../components/layouts/main';
+import DefaultLayout from '../components/layouts/default';
+import '../style.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    const Layout = Component.Layout || DefaultLayout;
+
+    return (
+      <MainLayout>
+        <Layout>
+        <Component {...pageProps} />
+
+        </Layout>
+      </MainLayout>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-import Head from "next/head";
+import Link from "next";
 import JustSay from "../components/justsay";
+import Counter from "../components/counter";
+import Timer from "../components/timer";
 
 export default function Home() {
   return (
     <div>
-      <JustSay />
-      <JustSay />
-      <JustSay />
+      <title>Widget - Daytech Dashboard</title>
+      <h2 className="text-xl undifined">Widgets</h2>
+      <div className="pt-3">
+        <div className="md:flex md:flex-wrap md:mr-4">
+          <JustSay />
+          <Counter />
+          <Timer />
+        </div>
+      </div>
     </div>
   );
 }
